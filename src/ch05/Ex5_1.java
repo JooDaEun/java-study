d8afd9a04d79b119b54d31997225cd8ae3d57ed0
JooDaEun <y0825131@naver.com>
2023-01-16 22:43:14 +0900

    chapter5

--- src/ch05/Ex5_1.java
+++ src/ch05/Ex5_1.java
@@ -1,4 +1,31 @@
 package ch05;
 
-public class Ex5_1 {
+import java.util.Arrays;
+class Ex5_1 {
+    public static void main(String[] args) {
+        int[] iArr1 = new int[10];
+        int[] iArr2 = new int[10];
+//		int[] iArr3 = new int[]{100, 95, 80, 70, 60};
+        int[] iArr3 = {100, 95, 80, 70, 60};
+        char[] chArr = {'a', 'b', 'c', 'd'};
+
+        for (int i = 0; i < iArr1.length ; i++ ) {
+            iArr1[i] = i + 1;                           // 1~10의 숫자를 순서대로 iArr1에 저장
+        }
+
+        for (int i = 0; i < iArr2.length ; i++ ) {
+            iArr2[i] = (int)(Math.random() * 10) + 1;   // 1~10의 랜덤값을 iArr2에 저장
+        }
+
+        // iArr1에 저장된 값 출력
+        for(int i = 0; i < iArr1.length ; i++) {
+            System.out.print(iArr1[i]+",");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(iArr2));
+        System.out.println(Arrays.toString(iArr3));
+        System.out.println(Arrays.toString(chArr));
+        System.out.println(iArr3);
+        System.out.println(chArr);
+    }
 }
---
+++ src/ch05/Ex5_8.java
@@ -0,0 +1,31 @@
+package ch05;
+
+class Ex5_8 {
+    public static void main(String[] args) {
+        int[][] score = {
+            { 100, 100, 100 }, { 20, 20, 20 }, { 30, 30, 30 }, { 40, 40, 40 }
+        };
+        int sum = 0;
+
+        for (int i = 0 ; i < score.length ; i++) {
+            for (int j = 0 ; j < score[i].length ; j++) {
+                System.out.printf("score[%d][%d]=%d%n", i, j, score[i][j]);
+                //score[0][0]=100
+                //score[0][1]=100
+                //score[0][2]=100
+                //score[1][0]=20
+                //score[1][1]=20
+                //score[1][2]=20
+                //score[2][0]=30
+                //score[2][1]=30
+                //score[2][2]=30
+                //score[3][0]=40
+                //score[3][1]=40
+                //score[3][2]=40
+                sum += score[i][j];
+            }
+        }
+        System.out.println("sum = " + sum);
+        //sum = 570
+    }
+}
